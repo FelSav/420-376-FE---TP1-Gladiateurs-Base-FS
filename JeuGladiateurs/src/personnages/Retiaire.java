9ddf8331f2d7c8a348454aae119d0df3395e1d86
202133255 <202133255@PO-24850>
2024-09-19 14:08:18 -0400

    Retiaire = Done

--- JeuGladiateurs/src/personnages/Retiaire.java
+++ JeuGladiateurs/src/personnages/Retiaire.java
@@ -1,8 +1,10 @@
 package personnages;
 
+import java.util.Random;
+
 public class Retiaire extends Personnage {
     
-    private boolean siFilet;
+    private boolean siFiletMain;
 
     public Retiaire(String nom, int attaqueMax, int defense, int pvs, int ini) {
         super(nom, attaqueMax, defense, pvs, ini);
@@ -23,5 +25,28 @@ public class Retiaire extends Personnage {
         }
     }
     
+    private boolean lancerFilet() {
+        Random rand = new Random();
+        return rand.nextInt(10) == 1;
+    }
     
+    @Override
+    public void frapperPersonnage(Personnage personnageCible) {
+        if (siFiletMain) {
+            System.out.println(nom + " lance son filet !");
+            
+            if (lancerFilet()) {
+                System.out.println("Son filet attrape " + personnageCible.getNom() + " et il l'ampale sauvagement avec sa lance !");
+                personnageCible.setPvs(0);
+            }else {
+                System.out.println("Le filet n'atteint pas sa cible !");
+            }
+            
+            siFiletMain = false;
+        }else {
+            System.out.println(nom + " ramasse son filet et en profite pour attaquer !");
+            super.frapperPersonnage(personnageCible);
+            siFiletMain = true;
+        }
+    }
 }
